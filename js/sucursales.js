b5afb11cf59f665fb25317901471bed68e9ac908
JoacoRodriguez <joacorm42@gmail.com>
2022-09-28 18:27:57 -0300

    Update sucursales.js

--- js/sucursales.js
+++ js/sucursales.js
@@ -40,23 +40,23 @@ $("#cuerpo").click(function (e) {
 
 //si obtengo los datos de la sucursal de un objeto local
 
-for ( const sucursal of sucursales) {
+// for ( const sucursal of sucursales) {
 
-    contenido += 
-    `<div class="card" id=${sucursal.id}>
-        <div class="card__img__box" id="cardImgBox"><img class="card__img" src=${sucursal.imagen} alt=""></div>
-        <div class="card__contenido">
-            <div class="card__datos">
-                <h3 class="card__title" id="cardTitle">${sucursal.nombre}</h3>
-                <p class="card__direccion" id="cardDireccion"> ${sucursal.direccion} </p>
-            </div>
-            <button class="card__boton">Pedir aquí</button>
-        </div>
-    </div>`
-}
+//     contenido += 
+//     `<div class="card" id=${sucursal.id}>
+//         <div class="card__img__box" id="cardImgBox"><img class="card__img" src=${sucursal.imagen} alt=""></div>
+//         <div class="card__contenido">
+//             <div class="card__datos">
+//                 <h3 class="card__title" id="cardTitle">${sucursal.nombre}</h3>
+//                 <p class="card__direccion" id="cardDireccion"> ${sucursal.direccion} </p>
+//             </div>
+//             <button class="card__boton">Pedir aquí</button>
+//         </div>
+//     </div>`
+// }
 
 
-$("#cuerpo").append(contenido);
+// $("#cuerpo").append(contenido);
 
 
 
